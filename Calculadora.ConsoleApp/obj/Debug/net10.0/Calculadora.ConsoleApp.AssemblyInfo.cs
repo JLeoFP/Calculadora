0ccd0d17ad77b8ef4178b838452b03b257a40535
Leo Fzp <Fernandezpatrijuanl93@gmail.com>
2026-03-05 14:28:49 -0300

    v2

--- Calculadora.ConsoleApp/obj/Debug/net10.0/Calculadora.ConsoleApp.AssemblyInfo.cs
+++ Calculadora.ConsoleApp/obj/Debug/net10.0/Calculadora.ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bef9ab1c78bc6cc7ad641dbcf410db6352e7ffa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
